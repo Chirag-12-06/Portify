30d78e1c90f51a18b8d3959a45cfa08bb7d9635a
Chirag Gupta <chirag.gupta.2120@gmail.com>
2026-08-07 10:18:58 +0530

    deployment issue resolved

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://portify-backend-5xiz.onrender.com"],
     credentials: true,
   }),
 );
